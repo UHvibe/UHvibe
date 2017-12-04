db3d0c58e6f1f261232f9e633931bb2bfbb8d714
jokabe <okabej3@gmail.com>
2017-12-03 18:44:50 -1000

    issue-45-(messaging-backend-code)
    1)Finished with Basic Layout of the read-message-page.html and it's functionality in read-message-page.js
    2)send-message-page.html and send-message-page.js is in progress

--- app/imports/ui/pages/messages/read-message-page.js
+++ app/imports/ui/pages/messages/read-message-page.js
@@ -58,7 +58,23 @@ Template.Read_Message_Page.helpers({
 
 Template.Read_Message_Page.events({
   'click #reply'(event, instance) {
-    console.log('reply');
+    const newDate = new Date();
+    const username = FlowRouter.getParam('username');
+    const destination = 'In Progress';
+    const date = newDate.toString();
+    const subject = 'In Progress';
+    const content = 'In Progress';
+    const newMessage = Message.define({
+      username: username,
+      destination: destination,
+      date: date,
+      subject: subject,
+      content: content
+    });
+    FlowRouter.go('/:username/messages/sendMessage/:messageID', {
+      username: FlowRouter.getParam('username'),
+      messageID: newMessage
+    });
   },
   'click #delete'(event, instance) {
     const messages = Message.findAll();
@@ -68,9 +84,13 @@ Template.Read_Message_Page.events({
         Message.removeIt( { _id: id } );
       }
     }
-    FlowRouter.go('/:username/messages', { username: FlowRouter.getParam('username') })
+    FlowRouter.go('/:username/messages', {
+      username: FlowRouter.getParam('username')
+    });
   },
   'click #back'(event, instance) {
-    FlowRouter.go('/:username/messages', { username: FlowRouter.getParam('username') })
+    FlowRouter.go('/:username/messages', {
+      username: FlowRouter.getParam('username')
+    });
   }
 });
---
+++ app/imports/ui/pages/messages/send-message-page.js
@@ -0,0 +1,82 @@
+import { Template } from 'meteor/templating';
+import { Message } from '../../../api/message/MessageCollection.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { _ } from 'meteor/underscore';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+Template.Send_Message_Page.onCreated(function onCreated() {
+  this.subscribe(Message.getPublicationName());
+});
+
+Template.Read_Message_Page.helpers({
+  sender() {
+    const messages = Message.findAll();
+    const id = FlowRouter.getParam('messageID');
+    for(let i = 0; i < messages.length; i++) {
+      if(id === messages[i]._id) {
+        return messages[i].username;
+      }
+    }
+  },
+  receiver() {
+    const messages = Message.findAll();
+    const id = FlowRouter.getParam('messageID');
+    for(let i = 0; i < messages.length; i++) {
+      if(id === messages[i]._id) {
+        return messages[i].destination;
+      }
+    }
+  },
+  subject() {
+    const messages = Message.findAll();
+    const id = FlowRouter.getParam('messageID');
+    for(let i = 0; i < messages.length; i++) {
+      if(id === messages[i]._id) {
+        return messages[i].subject;
+      }
+    }
+  },
+  messageContent() {
+    const messages = Message.findAll();
+    const id = FlowRouter.getParam('messageID');
+    for(let i = 0; i < messages.length; i++) {
+      if(id === messages[i]._id) {
+        return messages[i].content;
+      }
+    }
+  },
+});
+
+Template.Read_Message_Page.events({
+  'click #reply'(event, instance) {
+    const newDate = new Date();
+    const username = FlowRouter.getParam('username');
+    const destination = 'In Progress';
+    const date = newDate.toString();
+    const subject = 'In Progress';
+    const content = 'In Progress';
+    const newMessage = Message.define({
+      username: username,
+      destination: destination,
+      date: date,
+      subject: subject,
+      content: content
+    });
+    FlowRouter.go('/:username/messages/sendMessages/:messageID', {
+      username: FlowRouter.getParam('username'),
+      messageID: newMessage
+    });
+  },
+  'submit .send-message-form #cancel'(event, instance) {
+    const messages = Message.findAll();
+    const id = FlowRouter.getParam('messageID');
+    for(let i = 0; i < messages.length; i++) {
+      if(id === messages[i]._id) {
+        Message.removeIt( { _id: id } );
+      }
+    }
+    FlowRouter.go('/:username/messages', {
+      username: FlowRouter.getParam('username')
+    });
+  }
+});
