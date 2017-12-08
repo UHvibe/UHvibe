82f03a6b050857549e178157137455756c7a2c0f
jokabe <okabej3@gmail.com>
2017-12-08 01:36:20 -1000

    issue-18-(show sent messages or received messages, but not both)
    1)updated the reactive-dict
    2)changed the received/sent buttons to select options
    3)added reactive-dict object to allow live updates of the message table when the user selects option(received or sent)

--- app/imports/ui/pages/messages/messages-page.js
+++ app/imports/ui/pages/messages/messages-page.js
@@ -1,22 +1,40 @@
-import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Meteor } from 'meteor/meteor';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Template } from 'meteor/templating';
 import { Message } from '../../../api/message/MessageCollection.js';
 
-Template.Message_Page.onCreated(function onCreated() {
+Template.Message_Page.onCreated(function () {
   this.subscribe(Message.getPublicationName());
+  this.receivedMessage = new ReactiveDict();
+  this.receivedMessage.set('showReceivedMessages', true);
 });
 
 Template.Message_Page.helpers({
-  messageList() {
-    const id = FlowRouter.getParam('username');
-    return Message.find({
-      $or: [
-        { destination: id },
-        { username: id },
-      ],
-    });
+  receivedMessageList() {
+    console.log('receivemessage');
+    const id = Meteor.user().profile.name;
+    return Message.find({ destination: id });
+  },
+  sentMessageList() {
+    console.log('sentmessage');
+    const id = Meteor.user().profile.name;
+    return Message.find({ username: id });
   },
   routeUserName() {
-    return FlowRouter.getParam('username');
+    return Meteor.user().profile.name;
+  },
+  showReceivedMessages() {
+    return Template.instance().receivedMessage.get('showReceivedMessages');
+  },
+});
+
+Template.Message_Page.events({
+  'change select'(event) {
+    if ($(event.target).val() === 'receive') {
+      Template.instance().receivedMessage.set('showReceivedMessages', true);
+    }
+    if ($(event.target).val() === 'sent') {
+      Template.instance().receivedMessage.set('showReceivedMessages', false);
+    }
   },
 });
